4e348ee845a9e186d20d15cbd5d66dc1dc5a88f0
Harry <harryzzp@gmail.com>
2019-02-16 17:54:14 +0800

    update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,16 +10,13 @@ import Home from './Home';
 // and /schedule routes will match an y pathname that starts
 // with /roster or /schedule. The / route will only match
 // when the pathname is exactly the string "/"
-// 注意这个神奇的小写main，如果M大写则报错-_-!!!暂时没找到原因
 const Main = () => (
-      <main>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/trade" component={Trade} />
-          <Route path="/strategy" component={Strategy} />
-          <Route path="/gateway" component={Gateway} />
-        </Switch>
-      </main>
-    )
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route path="/trade" component={Trade} />
+    <Route path="/strategy" component={Strategy} />
+    <Route path="/gateway" component={Gateway} />
+  </Switch>
+)
 
 export default Main;
