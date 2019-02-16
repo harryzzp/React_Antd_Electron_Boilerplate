1ec520fafc76013c298017f7fb4b27ed8828807a
Harry <harryzzp@gmail.com>
2019-02-16 18:20:58 +0800

    main

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,12 +11,14 @@ import Home from './Home';
 // with /roster or /schedule. The / route will only match
 // when the pathname is exactly the string "/"
 const Main = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route path="/trade" component={Trade} />
-    <Route path="/strategy" component={Strategy} />
-    <Route path="/gateway" component={Gateway} />
-  </Switch>
+  <main>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/trade" component={Trade} />
+      <Route path="/strategy" component={Strategy} />
+      <Route path="/gateway" component={Gateway} />
+    </Switch>
+  </main>
 )
 
 export default Main;
