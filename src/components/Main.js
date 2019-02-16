be0c1aa2fc94d0c9fa0bfed33956b27064156ad1
Harry <harryzzp@gmail.com>
2019-02-16 17:15:55 +0800

    folders

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Trade from './Trade';
-import Strategy from './Strategy';
-import Gateway from './Gateway';
+import Trade from './trade/Trade';
+import Strategy from './strategy/Strategy';
+import Gateway from './gateway/Gateway';
 import Home from './Home';
 
 // The Main component renders one of the three provided
 // Routes (provided that one matches). Both the /roster
-// and /schedule routes will match any pathname that starts
+// and /schedule routes will match an y pathname that starts
 // with /roster or /schedule. The / route will only match
 // when the pathname is exactly the string "/"
 // 注意这个神奇的小写main，如果M大写则报错-_-!!!暂时没找到原因
