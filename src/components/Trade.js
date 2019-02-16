aed87a56ac396975b31532d37142a49807d2cbf9
Harry <harryzzp@gmail.com>
2019-02-16 15:44:07 +0800

    test

--- src/components/Trade.js
+++ src/components/Trade.js
@@ -1,70 +1,55 @@
 import React, { Component } from 'react';
-import { LocaleProvider, DatePicker, Alert, Button, Tabs, message } from 'antd';
-import zhCN from 'antd/lib/locale-provider/zh_CN';
-import moment from 'moment';
-import { StickyContainer, Sticky } from 'react-sticky';
+import { Layout, Menu, Icon } from 'antd';
+import 'antd/dist/antd.css';
 
-const TabPane = Tabs.TabPane;
 
-moment.locale('zh-cn');
+const { SubMenu } = Menu;
+const { Content, Sider } = Layout;
 
 class Trade extends Component {
-    state = {
-        date: null
-      };
-      handleChange = date => {
-        message.info(`您选择的日期是: ${date.format("YYYY-MM-DD")}`);
-        this.setState({ date });
-      };
-
-    render() {
-        const { date } = this.state;
-
-
-    const renderTabBar = (props, DefaultTabBar) => (
-        <Sticky bottomOffset={80}>
-          {({ style }) => (
-            <DefaultTabBar
-              {...props}
-              style={{ ...style, zIndex: 1, background: "#fff" }}
-            />
-          )}
-        </Sticky>
-      );
-        return(
-            <div className="App">
-              <LocaleProvider locale={zhCN}>
-                <div style={{ width: 400, margin: "100px auto" }}>
-                  <DatePicker onChange={this.handleChange} />
-                  <div style={{ marginTop: 20 }}>
-                    <Alert
-                      message={`当前日期：${
-                        date ? date.format("YYYY-MM-DD") : "未选择"
-                      }`}
-                      type="success"
-                    />
-                  </div>
-                </div>
-              </LocaleProvider>
-              <div style={{ width: 400, margin: "100px auto" }}>
-                <Button type="primary">确定</Button>
+  render() {
+    return (
+      <div>
+      <Content style={{ padding: '0 5px' }}>
+                  <Layout style={{ padding: '5px 0', background: '#fff' }}>
+                      <Sider width={200} style={{ background: '#fff', overflow: 'auto', height: '100vh', position:"fixed", left:0 }}>
+                          <Menu mode="inline" defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} style={{ height: '100%' }}>
+                              <SubMenu key="sub1" title={<span>
+                                  <Icon type="user" />股票</span>}>
+                                  <Menu.Item key="1">个股闪电手</Menu.Item>
+                                  <Menu.Item key="2">组合闪电手</Menu.Item>
+                                  <Menu.Item key="3">触发条件策略</Menu.Item>
+                                  <Menu.Item key="4">自动交易策略</Menu.Item>
+                                  <Menu.Item key="5">批量新股申购</Menu.Item>
+                              </SubMenu>
+                              <SubMenu key="sub2" title={<span>
+                                  <Icon type="laptop" />期货</span>}>
+                                  <Menu.Item key="6">option6</Menu.Item>
+                                  <Menu.Item key="7">option7</Menu.Item>
+                                  <Menu.Item key="8">option8</Menu.Item>
+                              </SubMenu>
+                              <SubMenu key="sub3" title={<span>
+                                  <Icon type="notification" />期权</span>}>
+                                  <Menu.Item key="9">option9</Menu.Item>
+                                  <Menu.Item key="10">option10</Menu.Item>
+                                  <Menu.Item key="11">option11</Menu.Item>
+                                  <Menu.Item key="12">option12</Menu.Item>
+                              </SubMenu>
+                              <SubMenu key="sub4" title={<span>
+                                  <Icon type="notification" />基金</span>}>
+                                  <Menu.Item key="13">FOF</Menu.Item>
+                                  <Menu.Item key="14">MOM</Menu.Item>
+                              </SubMenu>
+                          </Menu>
+                      </Sider>
+                      <Content style={{ padding: '0 5px', height:'100vh', position:"fixed", left:200 }}>
+                          Content
+                      </Content>
+                  </Layout>
+              </Content>
               </div>
-              <StickyContainer>
-                <Tabs defaultActiveKey="1" renderTabBar={renderTabBar}>
-                  <TabPane tab="Tab 1" key="1" style={{ height: 200 }}>
-                    Content of Tab Pane 1
-                  </TabPane>
-                  <TabPane tab="Tab 2" key="2">
-                    Content of Tab Pane 2
-                  </TabPane>
-                  <TabPane tab="Tab 3" key="3">
-                    Content of Tab Pane 3
-                  </TabPane>
-                </Tabs>
-              </StickyContainer>
-            </div>
-        );
-    }
+    );
+  }
 }
 
 export default Trade;
--- src/pages/App.js
+++ src/pages/App.js
@@ -2,67 +2,30 @@ import React, { Component } from 'react';
 import { Layout, Menu, Icon } from 'antd';
 import 'antd/dist/antd.css';
 import './App.css';
+import Trade from '../components/Trade';
 
-const { SubMenu } = Menu;
-const { Header, Content, Sider } = Layout;
+const { Header} = Layout;
 
 class App extends Component {
 
   render() {
     return (
-      <div>
-                  <Header className="header">
-                      <div className="logo" />
-                      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} style={{ lineHeight: '64px' }}>
-                          <Menu.Item key="1">
-                              <Icon type="dashboard" />交易</Menu.Item>
-                          <Menu.Item key="2">
-                              <Icon type="form" />策略</Menu.Item>
-                          <Menu.Item key="3">
-                              <Icon type="api" />网关</Menu.Item>
-                          <Menu.Item key="4">
-                              <Icon type="setting" />配置</Menu.Item>
-                      </Menu>
-                  </Header>
-              <Content style={{ padding: '0 5px' }}>
-                  <Layout style={{ padding: '5px 0', background: '#fff' }}>
-                      <Sider width={200} style={{ background: '#fff', overflow: 'auto', height: '100vh', position:"fixed", left:0 }}>
-                          <Menu mode="inline" defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} style={{ height: '100%' }}>
-                              <SubMenu key="sub1" title={<span>
-                                  <Icon type="user" />股票</span>}>
-                                  <Menu.Item key="1">个股闪电手</Menu.Item>
-                                  <Menu.Item key="2">组合闪电手</Menu.Item>
-                                  <Menu.Item key="3">触发条件策略</Menu.Item>
-                                  <Menu.Item key="4">自动交易策略</Menu.Item>
-                                  <Menu.Item key="5">批量新股申购</Menu.Item>
-                              </SubMenu>
-                              <SubMenu key="sub2" title={<span>
-                                  <Icon type="laptop" />期货</span>}>
-                                  <Menu.Item key="6">option6</Menu.Item>
-                                  <Menu.Item key="7">option7</Menu.Item>
-                                  <Menu.Item key="8">option8</Menu.Item>
-                              </SubMenu>
-                              <SubMenu key="sub3" title={<span>
-                                  <Icon type="notification" />期权</span>}>
-                                  <Menu.Item key="9">option9</Menu.Item>
-                                  <Menu.Item key="10">option10</Menu.Item>
-                                  <Menu.Item key="11">option11</Menu.Item>
-                                  <Menu.Item key="12">option12</Menu.Item>
-                              </SubMenu>
-                              <SubMenu key="sub4" title={<span>
-                                  <Icon type="notification" />基金</span>}>
-                                  <Menu.Item key="13">FOF</Menu.Item>
-                                  <Menu.Item key="14">MOM</Menu.Item>
-                              </SubMenu>
-                          </Menu>
-                      </Sider>
-                      <Content style={{ padding: '0 5px', height:'100vh', position:"fixed", left:200 }}>
-                          Content
-                      </Content>
-                  </Layout>
-              </Content>
-              
-      </div>
+        <div>
+            <Header className="header">
+                <div className="logo" />
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} style={{ lineHeight: '64px' }}>
+                    <Menu.Item key="1">
+                        <Icon type="dashboard" />交易</Menu.Item>
+                    <Menu.Item key="2">
+                        <Icon type="form" />策略</Menu.Item>
+                    <Menu.Item key="3">
+                        <Icon type="api" />网关</Menu.Item>
+                    <Menu.Item key="4">
+                        <Icon type="setting" />配置</Menu.Item>
+                </Menu>
+            </Header>
+            <Trade />
+        </div>
     );
   }
 }
