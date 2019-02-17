4437d54412dae198509fce2cda2a4f7e4d98143f
Harry <harryzzp@gmail.com>
2019-02-17 20:57:40 +0800

    test fetch in Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Home = () => (
-  <div style={{textAlign: "center", marginTop: "64px"}}>
-    <h1>欢迎来到掘金联盟！</h1>
-  </div>
-)
+const API = 'https://hn.algolia.com/api/v1/search?query=';
+const DEFAULT_QUERY = 'redux';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hits: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(API + DEFAULT_QUERY).then(response => response.json()).then(data => this.setState({ hits: data.hits }))
+  }
+
+  render() {
+
+    const { hits } = this.state;
+
+    return (
+      <div style={{ textAlign: "center", marginTop: "64px" }}>
+        <h1>欢迎来到量化联盟！</h1>
+        <div>
+          {hits.map(hit =>
+            <div key={hit.objectID}>
+              <a href={hit.url} target="_blank" rel="noopener noreferrer">{hit.title}</a>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+
+}
 
 export default Home
