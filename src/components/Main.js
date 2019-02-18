08f490594ff7d647f3e789019f0b11f1b4dc3acd
Harry <harryzzp@gmail.com>
2019-02-18 16:35:24 +0800

    add Redirect for home page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Trade from './trade/Trade';
 import Strategy from './strategy/Strategy';
 import Gateway from './gateway/Gateway';
@@ -8,7 +8,10 @@ import Home from './Home';
 const Main = () => (
   <main>
     <Switch>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/">
+        <Redirect to="/home" />
+      </Route>
+      <Route path="/home" component={Home} />
       <Route path="/trade" component={Trade} />
       <Route path="/strategy" component={Strategy} />
       <Route path="/gateway" component={Gateway} />
