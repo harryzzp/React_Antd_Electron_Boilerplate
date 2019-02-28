c611c394939d10685340923cda37af2a32dc7a3e
Harry <harryzzp@gmail.com>
2019-02-28 21:39:59 +0800

    css

--- src/actions/user.js
+++ src/actions/user.js
@@ -11,7 +11,7 @@ export const login = data => dispatch => {
       type: constants.USER_LOGGED_IN,
       payload: data
     });
-  }, 500);
+  }, 1000);
 };
 
 export function logout() {
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,5 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Spin } from 'antd';
+import './Loading.css';
 
 export default function Loading() {
-  return <div>Logging you in...</div>
+  return (
+    <div className='example'>
+      <Spin size='large' />
+    </div>
+  );
 }
